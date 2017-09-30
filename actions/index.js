4c6224683ab322bcc82ea255f89e59b208f8248c
Serge Nevsky <serge@snevsky.com>
2017-09-30 16:42:19 -0500

    Expanded on details page

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,9 @@
 export const DATA_AVAILABLE = 'DATA_AVAILABLE';
 
 const data = [
-  { 'name': 'Serge', 'description': 'That is me!' },
-  { 'name': 'Sharmi', 'description': 'That is my wife!' },
-  { 'name': 'Misha', 'description': 'That is the Mish-boi!' }
+  { 'name': 'Serge', 'description': 'That is me!', 'touched': '', 'oneOnOned': '' },
+  { 'name': 'Sharmi', 'description': 'That is my wife!', 'touched': '2017-08-09 13:37:38', 'oneOnOned': '2017-08-09 13:37:38' },
+  { 'name': 'Misha', 'description': 'That is the Mish-boi!', 'touched': '', 'oneOnOned': '' }
 ];
 
 export function getData() {
--- components/Details.js
+++ components/Details.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
-import { Title, List, ListITem } from 'react-native-elements';
+import { List, ListItem } from 'react-native-elements';
 
 export default class Details extends Component {
   render() {
+    const { name, description, touched, oneOnOned } = this.props.navigation.state.params;
     return <ScrollView>
+
+      <List>
+        <ListItem
+          title="Name"
+          rightTitle={name ? name : ' '}
+          hideChevron
+        />
+        <ListItem
+          title="Description"
+          rightTitle={description ? description : ' '}
+          hideChevron
+        />
+      </List>
+
+      <List>
+        <ListItem
+          title="Last Touch"
+          rightTitle={touched ? touched : 'Never'}
+          hideChevron
+        />
+        <ListItem
+          title="Last One On One"
+          rightTitle={oneOnOned ? oneOnOned : 'Never'}
+          hideChevron
+        />
+      </List>
 
     </ScrollView>;
   }
