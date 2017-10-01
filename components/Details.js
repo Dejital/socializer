9490ce5e27b7580f34080a9402388e38fe0fd521
Serge Nevsky <serge@snevsky.com>
2017-09-30 21:29:00 -0500

    Add headers to details

--- components/Details.js
+++ components/Details.js
@@ -1,38 +1,54 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text, StyleSheet } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
 export default class Details extends Component {
   render() {
     const { name, description, touched, oneOnOned } = this.props.navigation.state.params;
-    return <ScrollView>
+    return (
+      <ScrollView>
 
-      <List>
-        <ListItem
-          title="Name"
-          rightTitle={name ? name : ' '}
-          hideChevron
-        />
-        <ListItem
-          title="Description"
-          rightTitle={description ? description : ' '}
-          hideChevron
-        />
-      </List>
+        <List>
+          <ListItem
+            title='Name'
+            rightTitle={name ? name : ' '}
+            textInput={true}
+            hideChevron
+          />
+          <ListItem
+            title='Summary'
+            textInput={true}
+            rightTitle={description ? description : ' '}
+            hideChevron
+          />
+        </List>
 
-      <List>
-        <ListItem
-          title="Last Touch"
-          rightTitle={touched ? touched : 'Never'}
-          hideChevron
-        />
-        <ListItem
-          title="Last One On One"
-          rightTitle={oneOnOned ? oneOnOned : 'Never'}
-          hideChevron
-        />
-      </List>
+        <Text style={styles.listHeader}>
+          CONTACT HISTORY
+        </Text>
+        <List>
+          <ListItem
+            title='Last Contact'
+            rightTitle={touched ? touched : 'Never'}
+            hideChevron
+          />
+          <ListItem
+            title='Last One on One'
+            rightTitle={oneOnOned ? oneOnOned : 'Never'}
+            hideChevron
+          />
+        </List>
 
-    </ScrollView>;
+      </ScrollView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  listHeader: {
+    color: 'gray',
+    marginTop: 20,
+    marginLeft: 20,
+    marginBottom: -10
+  }
+});
